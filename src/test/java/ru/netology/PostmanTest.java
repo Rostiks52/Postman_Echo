f62b5915455e20f4d3d64a3c0e8d2c73d4b4025a
Rostiks52 <ksyu_05@mail.ru>
2021-09-16 10:42:24 +0300

    Files added

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+        void shouldTryPostmanReqests(){
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .contentType(ContentType.JSON);
+    }
+}
