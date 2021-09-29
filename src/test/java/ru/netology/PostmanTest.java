77f19fb36e03c19a9d72cccad4b24eaa9acf9ee6
Rostiks52 <ksyu_05@mail.ru>
2021-09-29 12:10:00 +0300

    Test lfailured

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -13,10 +13,10 @@ class PostmanTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data")
-                .when()
+               .when()
                 .post("/post")
-                .then()
-                .statusCode(200)
+               .then()
+                .statusCode(300)
                 .body("data", equalTo("some data"))
                 .contentType(ContentType.JSON);
     }
